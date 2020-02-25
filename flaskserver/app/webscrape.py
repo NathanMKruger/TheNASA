0937e34f24d6bba1529f582f74b6bba6bfe9ad41
NathanMKruger <35467462+NathanMKruger@users.noreply.github.com>
2020-02-25 15:01:09 -0500

    start of web scraping

---
+++ flaskserver/app/webscrape.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import requests
+
+def main():
+  site = input("Enter URL: ")
+  
+if __name__ == "__main__":
+  main()
+
+def getSiteTitle(site):
+  header = requests.utils.default_headers()
+  req = requests.get(site, header)
+  
+  soup = BeautifulSoup(req.content, 'html.parser')
+   
+  soup.prettify()
+
+  siteTitle = ''
+  
+  if site.find("title") is not None:
+    siteTitle = site.find("title")
+  elif site.find("h1") is not None:
+    siteTitle = link.find("h1")
+ 
+ return siteTitle
