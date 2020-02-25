a83d8a2319b7fe4bf42ad99a4941383cee556dbf
davidrharvey <david.harvey@mymail.champlain.edu>
2020-02-25 13:51:49 -0500

    Added basic text input

--- flaskserver/app/routes.py
+++ flaskserver/app/routes.py
@@ -1,6 +1,12 @@
+from flask import request, render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+def my_form():
+    return render_template('input.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
