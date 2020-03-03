4aa8aa6d22224ea61f68056d7d6be3604a0d789b
Scott Dagen <scott.dagen@mymail.champlain.edu>
2020-03-03 14:04:05 -0500

    Added second window

---
+++ client/detectorUI.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\Scott\Dev\TheNASA\client\detector.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_DetectorWindow(QtWidgets.QMainWindow):
+    def setupUi(self):
+        self.setObjectName("MainWindow")
+        self.resize(300, 300)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.sizePolicy().hasHeightForWidth())
+        self.setSizePolicy(sizePolicy)
+        self.setMinimumSize(QtCore.QSize(300, 280))
+        self.setMaximumSize(QtCore.QSize(300, 280))
+        self.centralwidget = QtWidgets.QWidget(self)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
+        self.centralwidget.setSizePolicy(sizePolicy)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(70, 10, 161, 51))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.label_2.setFont(font)
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_2.setObjectName("label_2")
+        self.line_2 = QtWidgets.QFrame(self.centralwidget)
+        self.line_2.setGeometry(QtCore.QRect(20, 60, 261, 20))
+        self.line_2.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line_2.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line_2.setObjectName("line_2")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(40, 90, 101, 16))
+        self.label.setAlignment(QtCore.Qt.AlignBottom|QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft)
+        self.label.setObjectName("label")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(160, 90, 121, 20))
+        self.lineEdit.setObjectName("lineEdit")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(70, 120, 161, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.line_3 = QtWidgets.QFrame(self.centralwidget)
+        self.line_3.setGeometry(QtCore.QRect(20, 150, 261, 20))
+        self.line_3.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line_3.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line_3.setObjectName("line_3")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(56, 170, 191, 21))
+        self.label_3.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(56, 200, 191, 20))
+        self.label_4.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_4.setObjectName("label_4")
+        self.line_4 = QtWidgets.QFrame(self.centralwidget)
+        self.line_4.setGeometry(QtCore.QRect(20, 220, 261, 20))
+        self.line_4.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line_4.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line_4.setObjectName("line_4")
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(110, 240, 71, 21))
+        self.label_5.setObjectName("label_5")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(184, 240, 101, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi()
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+    def retranslateUi(self):
+        _translate = QtCore.QCoreApplication.translate
+        self.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label_2.setText(_translate("MainWindow", "Clickbait Detector"))
+        self.label.setText(_translate("MainWindow", "Enter a title or URL:"))
+        self.pushButton.setText(_translate("MainWindow", "Submit"))
+        self.label_3.setText(_translate("MainWindow", "{Name}"))
+        self.label_4.setText(_translate("MainWindow", "is {CLICKBAIT/NOT CLICKBAIT (%)}"))
+        self.label_5.setText(_translate("MainWindow", "Is this wrong?"))
+        self.pushButton_2.setText(_translate("MainWindow", "Submit Correction"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    ui = Ui_DetectorWindow()
+    ui.setupUi()
+    ui.show()
+    sys.exit(app.exec_())
--- client/ui.py
+++ client/ui.py
@@ -8,49 +8,65 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from detectorUI import Ui_DetectorWindow
 
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(600, 300)
+class Ui_MainWindow(QtWidgets.QMainWindow):
+    
+    def setupUi(self):
+        self.setObjectName("MainWindow")
+        self.resize(600, 300)
+        
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
-        MainWindow.setSizePolicy(sizePolicy)
-        MainWindow.setMinimumSize(QtCore.QSize(600, 300))
-        MainWindow.setMaximumSize(QtCore.QSize(600, 300))
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        sizePolicy.setHeightForWidth(self.sizePolicy().hasHeightForWidth())
+        self.setSizePolicy(sizePolicy)
+        
+        self.setMinimumSize(QtCore.QSize(600, 300))
+        self.setMaximumSize(QtCore.QSize(600, 300))
+        
+        self.centralwidget = QtWidgets.QWidget(self)
+        
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
+        
         self.centralwidget.setSizePolicy(sizePolicy)
         self.centralwidget.setObjectName("centralwidget")
+        
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(70, -10, 441, 91))
+        
         font = QtGui.QFont()
         font.setPointSize(16)
         font.setBold(True)
         font.setWeight(75)
+        
         self.label.setFont(font)
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
+        
         self.line = QtWidgets.QFrame(self.centralwidget)
         self.line.setGeometry(QtCore.QRect(0, 70, 801, 20))
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Fixed)
+        
         sizePolicy.setHorizontalStretch(1)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.line.sizePolicy().hasHeightForWidth())
+        
         self.line.setSizePolicy(sizePolicy)
         self.line.setFrameShape(QtWidgets.QFrame.HLine)
         self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.line.setObjectName("line")
+        
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setGeometry(QtCore.QRect(220, 80, 161, 51))
+        
         font = QtGui.QFont()
         font.setPointSize(14)
+        
         self.label_2.setFont(font)
         self.label_2.setAlignment(QtCore.Qt.AlignCenter)
         self.label_2.setObjectName("label_2")
@@ -64,44 +80,59 @@ class Ui_MainWindow(object):
         self.btnEmpty_1.setEnabled(False)
         self.btnEmpty_1.setGeometry(QtCore.QRect(190, 170, 221, 23))
         self.btnEmpty_1.setObjectName("btnEmpty_1")
+        
         self.line_2 = QtWidgets.QFrame(self.centralwidget)
         self.line_2.setGeometry(QtCore.QRect(0, 120, 801, 20))
         self.line_2.setFrameShape(QtWidgets.QFrame.HLine)
         self.line_2.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.line_2.setObjectName("line_2")
+        
         self.btnEmpty_2 = QtWidgets.QPushButton(self.centralwidget)
         self.btnEmpty_2.setEnabled(False)
         self.btnEmpty_2.setGeometry(QtCore.QRect(190, 200, 221, 23))
         self.btnEmpty_2.setObjectName("btnEmpty_2")
+        
         self.btnEmpty_3 = QtWidgets.QPushButton(self.centralwidget)
         self.btnEmpty_3.setEnabled(False)
         self.btnEmpty_3.setGeometry(QtCore.QRect(190, 230, 221, 23))
         self.btnEmpty_3.setObjectName("btnEmpty_3")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        
+        self.setCentralWidget(self.centralwidget)
+        
+        self.menubar = QtWidgets.QMenuBar(self)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 600, 21))
         self.menubar.setObjectName("menubar")
+        
         self.menuFile = QtWidgets.QMenu(self.menubar)
         self.menuFile.setObjectName("menuFile")
-        MainWindow.setMenuBar(self.menubar)
-        self.actionHome = QtWidgets.QAction(MainWindow)
+        
+        self.setMenuBar(self.menubar)
+        
+        self.actionHome = QtWidgets.QAction(self)
         self.actionHome.setObjectName("actionHome")
-        self.actionClickbait_Detector = QtWidgets.QAction(MainWindow)
+        
+        self.actionClickbait_Detector = QtWidgets.QAction(self)
         self.actionClickbait_Detector.setObjectName("actionClickbait_Detector")
+        
         self.menuFile.addAction(self.actionHome)
         self.menuFile.addSeparator()
         self.menuFile.addAction(self.actionClickbait_Detector)
+        self.menuFile.triggered[QtWidgets.QAction].connect(self.displayDetector)
+        
         self.menubar.addAction(self.menuFile.menuAction())
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.retranslateUi()
+        QtCore.QMetaObject.connectSlotsByName(self)
 
-    def displayDetector(self):
-        print("Placeholder!")
+        self.dialog = Ui_DetectorWindow()
 
-    def retranslateUi(self, MainWindow):
+    def displayDetector(self, q = None):
+        self.dialog.setupUi()
+        self.dialog.show()
+
+    def retranslateUi(self):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.label.setText(_translate("MainWindow", "The National Agile and Scrum Association"))
         self.label_2.setText(_translate("MainWindow", "Our Projects"))
         self.btnDetector.setText(_translate("MainWindow", "Clickbait Detector"))
@@ -116,8 +147,7 @@ class Ui_MainWindow(object):
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    ui.setupUi()
+    ui.show()
     sys.exit(app.exec_())
